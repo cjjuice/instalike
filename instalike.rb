e8cb0f74d1c6b78aa56c111ff9214d539fd3f6cb
C.J. Bordeleau <bordeleau.cj@gmail.com>
2013-01-26 13:18:21 -0500

    improve error handling

--- instalike.rb
+++ instalike.rb
@@ -29,7 +29,9 @@ selected_tags.each do |tag|
   
   #get 60 most recent photos from tag
   photos = Instagram.tag_recent_media(tag, {count: 60})
-                                              
+
+  error_count = 0
+                                            
   photos.data.each do |photo|
     # like each photo 
     puts "starting liking #{tag} - #{photo.id}"
@@ -37,8 +39,12 @@ selected_tags.each do |tag|
       Instagram.like_media(photo.id)
       puts "completed liking #{photo.id}"
     rescue Exception => e  
-      puts e.message  
-      sleep 600  
+      error_count = error_count + 1
+      puts e.message 
+      if error_count >= 2   
+        puts 'Error! Retry has failed, waiting 10 min'
+        sleep 600
+      end 
     end
   end
 end 
